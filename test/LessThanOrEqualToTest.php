2ec395d097f93a90cc5319225ecb1fb6814d592f
Jakub Giminski <jakub@gigabyte.software>
2019-08-05 00:54:24 +0100

    Add another test

---
+++ test/LessThanOrEqualToTest.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Comquer\ComparatorTest;
+
+use Comquer\CriteriaEvaluator\Criteria\LessThanOrEqualTo;
+use PHPUnit\Framework\TestCase;
+
+class LessThanOrEqualToTest extends TestCase
+{
+    /** @test */
+    function evaluation()
+    {
+        $isValueLessThanOrEqualToFive = new LessThanOrEqualTo(5);
+        self::assertTrue($isValueLessThanOrEqualToFive(4));
+        self::assertTrue($isValueLessThanOrEqualToFive(5));
+        self::assertFalse($isValueLessThanOrEqualToFive(6));
+    }
+
+    /** @test */
+    function serialization()
+    {
+        $isValueLessThanOrEqualToFive = new LessThanOrEqualTo(5);
+        self::assertEquals(
+            $isValueLessThanOrEqualToFive,
+            LessThanOrEqualTo::deserialize($isValueLessThanOrEqualToFive->serialize())
+        );
+    }
+}
